25ec203b0c2c7fab537c9b355d422eac5deb608a
jentranthao1111 <1234567811t@gmail.com>
2021-10-17 03:07:32 -0400

    2021-09-14:
    /*2021-09-14:  Create listener for the options - answer and create the toast message */

--- app/src/main/java/com/example/mathquizz/controller/QuizzActivity.java
+++ app/src/main/java/com/example/mathquizz/controller/QuizzActivity.java
@@ -13,35 +13,32 @@ import com.example.mathquizz.model.AnalyzeQuestion;
 import com.example.mathquizz.model.Question;
 import com.example.mathquizz.model.QuestionRepository;
 
-import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public class QuizzActivity extends Activity {
 
+    public static final String INDICE_QUESTION = "INDICE_QUESTION";
     private final QuestionRepository respository = new QuestionRepository();
+    private final Locale locale = new Locale("en", "CA");
     private int indice_question = 0;
+    private TextView textViewTextoPergunta ;
+    int maxQuestion = respository.getListQuestions().size();
+    private Button btnAns1;
+    private Button btnAns2;
+    private Button btnResult;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // class utility
-        Question question = respository.getListQuestions().get(0);
 
-        //listener for btnNextQuestion
-        View.OnClickListener listenerQuestion = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                for(int i = indice_question; i == respository.getListQuestions().size(); i ++){
-                    Question question = respository.getListQuestions().get(i+1);
-                }
-            }
-        };
+        Question question = respository.getListQuestions().get(indice_question);
 
         // R - class utility resources, id - what to access, txtQuestion - id of obj
-        TextView textViewTextoPergunta = findViewById(R.id.text_question_textview);
-        textViewTextoPergunta.setText(question.getText());
+        textViewTextoPergunta = findViewById(R.id.text_question_textview);
 
         // need to instantiate listener
         View.OnClickListener listener = new View.OnClickListener(){ // connect main activity - view to the code
@@ -54,43 +51,89 @@ public class QuizzActivity extends Activity {
                 String message;
                 Question question = respository.getListQuestions().get(0);
 
-                if (analyzeQuestion.isRespotaCorrecta( question, Double.valueOf(reposta)) ){
-                    message = "Congrats , Correct Answer!";
+                try {
+                    // NumberFormat is a java abstract class for all the number
+                    NumberFormat format = NumberFormat.getCurrencyInstance(locale); // instantiate format as a var to store currency of the locale
+                    // Number is a java abstract class that represent values that are automatically convertible to format below
+                    Number number = format.parse(reposta);
+                    if (analyzeQuestion.isRespotaCorrecta(question, number.doubleValue())) {
+                        message = "Congrats , Correct Answer!";
 
-                }else{
-                    message = "Ahh, Wrong Answer";
-                }
+                    } else {
+                        message = "Ahh, Wrong Answer";
+                    }
+
+                }catch (ParseException e){
+                    message = e.getMessage();
+                };
                 Toast.makeText(QuizzActivity.this, message, Toast.LENGTH_SHORT).show();
 
             } // end function OnClick
         }; // end instantiate listener for options
 
 
-
-
-        Button btnAns1 = findViewById(R.id.option1_button); // this button has a variable named btnAns1
+        btnAns1 = findViewById(R.id.option1_button); // this button has a variable named btnAns1
         btnAns1.setText(String.valueOf(question.getRespostaCorrecta()));
         btnAns1.setOnClickListener(listener);
 
-        Button btnAns2 = findViewById(R.id.option2_button);
+        btnAns2 = findViewById(R.id.option2_button);
         btnAns2.setText(String.valueOf(question.getRespostaIncorreta()));
         btnAns2.setOnClickListener(listener);
 
+        //listener for btnNextQuestion
+        View.OnClickListener listenerNextQuestion = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                indice_question ++;
+                if (indice_question >= maxQuestion){
+                   indice_question = 0;
+                }
+               exibirQuestao(indice_question);
+            } // end func onclick
+        }; // end instantiate listener for next question
 
+        btnResult = findViewById(R.id.proxima_question_button);
+        btnResult.setOnClickListener(listenerNextQuestion);
 
-        Button btnResult = findViewById(R.id.proxima_question_button);
-        btnResult.setText("Next Question");
-        btnResult.setOnClickListener(listenerQuestion);
-
-
-        //Listeners are interfaces that connect the button with his interface that we code
-        // that meaning is what will happen when the button is clicked - it is also called as events
-
+        if(savedInstanceState != null){
+            indice_question = savedInstanceState.getInt(INDICE_QUESTION );
+            //indice_question = savedInstanceState.getInt()
+        }
+        exibirQuestao(indice_question);
 
     }// end onCreate
-    /*2021-09-07: Create the Question class,
+
+    // to reload the previous data in case the application is crashed/ exploded
+    protected void onSaveInstanceState(Bundle outState){ // bundle is out of its state - being exploded
+       // super.onSaveInstanceState(INDICE_QUESTION,  indice_question);
+       super.onSaveInstanceState(outState);
+       outState.putInt(INDICE_QUESTION, indice_question);
+    }
+    public void exibirQuestao (final int indice_question){
+        Question question = respository.getListQuestions().get(indice_question);
+
+        textViewTextoPergunta.setText(question.getText());
+
+        String repostaCorreta = String.format(locale," %.2f", question.getRespostaCorrecta());
+        String repostaIncorreta = String.format(locale," %.2f", question.getRespostaIncorreta());
+
+        btnAns1.setText(String.valueOf(question.getRespostaIncorreta()));
+        btnAns2.setText(String.valueOf(question.getRespostaIncorreta()));
+    }
+
+}// end class
+
+//Listeners are interfaces that connect the button with his interface that we code
+// that meaning is what will happen when the button is clicked - it is also called as events
+
+/*2021-09-07: Create the Question class,
     QuizzActiivty for the Listener,
     QuestionRespository for List<Question>,
     AnalyzeQuestion to return correct answer*/
 
-}// end class
+/*2021-09-14:  Create listener for the options - answer and create the toast message */
+
+/*2021-09-16:  Create listener for the result - "next question",
+               Optimize the code by using function and private variables */
+
+/*2021-09-21: explaining abt bundle, onSaveInstanceState */
